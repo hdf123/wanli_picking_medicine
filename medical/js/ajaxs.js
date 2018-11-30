754d9f6b8cdaf86a6e3fd58719c4d1617b75a0e1
cc <15137102387@163.com>
2018-11-30 17:54:34 +0800

    011

--- medical/js/ajaxs.js
+++ medical/js/ajaxs.js
@@ -15,8 +15,8 @@
 	
 	
 //ajax封装调用
-//	var urs="39.104.127.252:8080/";
-	var urs="http://192.168.1.126:8082/";
+	var urs="http://39.104.127.252:8080/wanlitiaoyi/";
+//	var urs="http://192.168.1.126:8082/";
 	var errorks="网络连接错误";
 	function ajaxsd(url,type,data,suFn,erFn){
 		$.ajax({
@@ -31,8 +31,12 @@
 			success: function(data) {
 				suFn(data);
 			},
-	        error: function(error) {
+	        error: function(error){
 	            erFn(error);
+	            return function(){
+	            	alert("登录已失效，请重新登录");
+	            	window.location.href="../login/login.html";
+	            }
 	        }
 		});
 	}
